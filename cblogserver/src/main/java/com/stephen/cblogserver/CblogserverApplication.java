059470ed2d8407f5923942630830cd00ca484c49
loveisacat <chenbin15@sina.com>
2020-05-27 15:34:43 +0800

    add server

---
+++ cblogserver/src/main/java/com/stephen/cblogserver/CblogserverApplication.java
@@ -0,0 +1,13 @@
+package com.stephen.cblogserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CblogserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CblogserverApplication.class, args);
+    }
+
+}
---
+++ cblogserver/src/test/java/com/stephen/cblogserver/CblogserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stephen.cblogserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CblogserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
